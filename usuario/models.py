6dbddba4916ad1e70b2fdfbd9daa63cd8f0dd7bc
M1Moya <113069047+M1Moya@users.noreply.github.com>
2023-11-12 23:44:16 -0300

    XD

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+#class AuthUser(models.Model):
+#    password = models.CharField(max_length=128)
+#    last_login = models.DateTimeField(blank=True, null=True)
+#    is_superuser = models.IntegerField()
+#    rut = models.CharField(unique=True, max_length=150)
+#    first_name = models.CharField(max_length=150)
+#    last_name = models.CharField(max_length=150)
+#    email = models.CharField(max_length=254)
+#    is_staff = models.IntegerField()
+#    is_active = models.IntegerField()
+#    date_joined = models.DateTimeField()
+
+class Ejemplo(models.Model):
+    nombre = models.CharField(blank=True, max_length=15, db_column='Nombre')
+    apellido = models.CharField(blank=True, max_length=15, db_column='Apellido')
