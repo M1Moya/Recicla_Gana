262e032974bcce655744d6262af5edd94f791748
M1Moya <113069047+M1Moya@users.noreply.github.com>
2023-11-15 02:58:13 -0300

    LIstView Recompensas al 100%

--- usuario/urls.py
+++ usuario/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 #Urls de app Usuario
 urlpatterns = [
-    path("recompensa/", ListRecompensaView.as_view(), name='recompensa')
+    path("recompensa/", ListRecompensaView.as_view(), name='recompensa'),
+    path("recompensa/agregar_recompensa/", NewRecompensa.as_view(), name='new_recompensa')    
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView, UpdateView
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import*
 
 # Create your views here.
 class ListRecompensaView(ListView):
     model = Canjeables
     template_name = "list_recompensa.html"
-    context_object_name = "canjeable"
+
+class NewRecompensa(CreateView):
+    model = Canjeables
+    template_name = "new_recompensa.html"
+    fields = ["recompensa", "categoria", "puntos"]
+    success_url = reverse_lazy("recompensa")
